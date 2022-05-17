ae52c6522e5715193e662490f9e443443b14ca0b
Fabrício Rangel de Sousa <fabricio3176@gmail.com>
2022-05-17 13:14:47 -0300

    Update proprietarioproRoutes.js

--- ORM/src/api/routes/proprietarioproRoutes.js
+++ ORM/src/api/routes/proprietarioproRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router ();
+const router = express.Router();
 
 const proprietarioproControllers = require ('../controllers/proprietarioproControllers');
 
